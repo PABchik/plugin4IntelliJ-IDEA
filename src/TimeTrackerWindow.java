5c1d4d6d488604d6c79de6ee819f57ddced400ef
Paul <webmoney91rus@yandex.ru>
2019-01-17 08:41:16 +0300

    Added actionListener for buttons play and stop

--- src/TimeTrackerWindow.java
+++ src/TimeTrackerWindow.java
@@ -1,18 +1,43 @@
 import com.intellij.openapi.wm.ToolWindow;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TimeTrackerWindow {
 
+    private TimeTracker timeTracker;
+
     private JPanel mainPanel;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
+    private JButton playB;
+    private JButton pauseB;
+    private JButton stopB;
     private JTextField yourCurrentTaskTextField;
-    private JButton hideButton;
+    private JButton hideB;
+    private JLabel time;
 
     public TimeTrackerWindow(ToolWindow toolWindow) {
 
+        timeTracker = new TimeTracker();
+
+        playB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!timeTracker.running) {
+                    timeTracker.start();
+                    timeTracker.printElapsedTime();
+                }
+            }
+        });
+
+        stopB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (timeTracker.running) {
+                    timeTracker.stop();
+                }
+            }
+        });
     }
 
     public JPanel getContent() {
